4fab909550282058ff71fda14b574e4a95e4f17d
Jeffery Allain <jefferyallain@gmail.com>
2021-02-01 00:51:49 -0600

    Added Rikishi selection table

--- projects/basho/src/app/pick-selector/pick-selector.component.ts
+++ projects/basho/src/app/pick-selector/pick-selector.component.ts
@@ -11,17 +11,40 @@ export class PickSelectorComponent implements OnInit {
 
   selected: Map<string,Rikishi> = new Map([
     ["A", DefaultRikishi],
-    ["B", { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi A", rank:"Ozeki" }],
-    ["C", { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi A", rank:"Ozeki" }],
+    ["B", DefaultRikishi],
+    ["C", DefaultRikishi],
     ["D", DefaultRikishi],
     ["E", DefaultRikishi]
   ]);
 
-  rikishis: Rikishi[] = [
-    { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi A", rank:"Ozeki" },
-    { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi B", rank:"Ozeki" },
-    { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi C", rank:"Ozeki" },
-  ]
+  rikishis: Map<string,Rikishi[]> = new Map([
+    ["A",[
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi A", rank:"Ozeki" },
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi B", rank:"Ozeki" },
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi C", rank:"Ozeki" }
+    ]],
+    ["B",[
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi D", rank:"Ozeki" },
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi E", rank:"Ozeki" },
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi F", rank:"Ozeki" }
+    ]],
+    ["C",[
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi G", rank:"Ozeki" },
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi H", rank:"Ozeki" },
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi I", rank:"Ozeki" }
+    ]],
+    ["D",[
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi J", rank:"Ozeki" },
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi K", rank:"Ozeki" },
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi L", rank:"Ozeki" }
+    ]],
+    ["E",[
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi M", rank:"Ozeki" },
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi N", rank:"Ozeki" },
+      { filled: true, avatar:'/assets/default_avatar.jpg', name:"Rikishi O", rank:"Ozeki" }
+    ]]
+  ]);
+
   constructor() { }
 
   ngOnInit(): void {
