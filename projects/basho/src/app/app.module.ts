be9461c483b4ad09c40979bf73b34a282738de89
Jeffery Allain <jefferyallain@gmail.com>
2021-02-03 19:26:10 -0600

    Adding auth-button with login

--- projects/basho/src/app/app.module.ts
+++ projects/basho/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AuthModule } from '@auth0/auth0-angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PickSelectorComponent } from './pick-selector/pick-selector.component';
+import { AuthButtonComponent } from './auth-button/auth-button.component';
 
 export const test = ["A","B","C"]
 
 @NgModule({
   declarations: [
     AppComponent,
-    PickSelectorComponent
+    PickSelectorComponent,
+    AuthButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'jefall.auth0.com',
+      clientId: 'aVBuQ4KDAaXgOLFi4M88FCTx3yAy3qLk'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
